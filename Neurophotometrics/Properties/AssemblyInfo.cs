a65abe53d85a9f89650ab7ef5b2979d131372833
glopesdev <glopesdev@users.noreply.github.com>
2020-05-19 19:53:15 +0100

    Update package project to the new csproj format

--- Neurophotometrics/Properties/AssemblyInfo.cs
+++ Neurophotometrics/Properties/AssemblyInfo.cs
@@ -1,37 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Bonsai;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neurophotometrics")]
-[assembly: AssemblyDescription("A Bonsai interface for data acquisition and control of Neurophotometrics devices.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Neurophotometrics Ltd.")]
-[assembly: AssemblyProduct("Neurophotometrics")]
-[assembly: AssemblyCopyright("Copyright © Neurophotometrics Ltd. 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("28eb1b06-b3db-45cf-a33c-4109eae1ce35")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: XmlNamespacePrefix("clr-namespace:Neurophotometrics", "npm")]
