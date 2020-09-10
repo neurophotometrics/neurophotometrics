4205c6592dc649f63f72fb80f65a4e5aaf5f3175
glopesdev <glopesdev@users.noreply.github.com>
2020-09-10 01:07:52 +0100

    Add minimum size to roi calibration dialog

--- Neurophotometrics.Design/FP3001CalibrationEditorForm.Designer.cs
+++ Neurophotometrics.Design/FP3001CalibrationEditorForm.Designer.cs
@@ -42,19 +42,21 @@
             this.tableLayoutPanel.Controls.Add(this.visualizerPanel, 0, 1);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 2;
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(689, 591);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(459, 378);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // calibrateRoiButton
             // 
             this.calibrateRoiButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.calibrateRoiButton.Location = new System.Drawing.Point(3, 3);
+            this.calibrateRoiButton.Location = new System.Drawing.Point(2, 2);
+            this.calibrateRoiButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.calibrateRoiButton.Name = "calibrateRoiButton";
-            this.calibrateRoiButton.Size = new System.Drawing.Size(683, 54);
+            this.calibrateRoiButton.Size = new System.Drawing.Size(455, 34);
             this.calibrateRoiButton.TabIndex = 0;
             this.calibrateRoiButton.Text = "Calibrate Regions";
             this.calibrateRoiButton.UseVisualStyleBackColor = true;
@@ -63,18 +65,21 @@
             // visualizerPanel
             // 
             this.visualizerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.visualizerPanel.Location = new System.Drawing.Point(3, 63);
+            this.visualizerPanel.Location = new System.Drawing.Point(2, 40);
+            this.visualizerPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.visualizerPanel.Name = "visualizerPanel";
-            this.visualizerPanel.Size = new System.Drawing.Size(683, 525);
+            this.visualizerPanel.Size = new System.Drawing.Size(455, 336);
             this.visualizerPanel.TabIndex = 1;
             // 
             // FP3001CalibrationEditorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 591);
+            this.ClientSize = new System.Drawing.Size(459, 378);
             this.Controls.Add(this.tableLayoutPanel);
             this.Icon = global::Neurophotometrics.Design.Properties.Resources.Neurophotometrics;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(470, 420);
             this.Name = "FP3001CalibrationEditorForm";
             this.Text = "FP3001 Setup";
             this.tableLayoutPanel.ResumeLayout(false);
