0a874fd52e7f8ad3df78de5654ed33335657f4e5
glopesdev <glopesdev@users.noreply.github.com>
2020-09-14 22:19:55 +0100

    Swap DIO labels in frame flags

--- Neurophotometrics/FrameFlags.cs
+++ Neurophotometrics/FrameFlags.cs
@@ -11,12 +11,12 @@ namespace Neurophotometrics
         L415 = 1 << 0,
         L470 = 1 << 1,
         L560 = 1 << 2,
-        Output0 = 1 << 3,
-        Output1 = 1 << 4,
+        Output1 = 1 << 3,
+        Output0 = 1 << 4,
         Stimulation = 1 << 5,
         Line2 = 1 << 6,
         Line3 = 1 << 7,
-        Input0 = 1 << 8,
-        Input1 = 1 << 9
+        Input1 = 1 << 8,
+        Input0 = 1 << 9
     }
 }
