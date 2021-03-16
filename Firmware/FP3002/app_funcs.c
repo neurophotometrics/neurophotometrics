09d05f263be44afa53c829532e3230c47f12aa2f
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2021-03-16 19:20:34 +0000

    Created new register to store camera's serial number.

--- Firmware/FP3002/app_funcs.c
+++ Firmware/FP3002/app_funcs.c
@@ -59,7 +59,7 @@ void (*app_func_rd_pointer[])(void) = {
 	&app_read_REG_SCREEN_ASSEMBLY_VERSION,
 	&app_read_REG_SCREEN_FW_VERSION_H,
 	&app_read_REG_SCREEN_FW_VERSION_L,
-	&app_read_REG_RESERVED1,
+	&app_read_REG_CAMERA_SN,
 	&app_read_REG_CAL_L410,
 	&app_read_REG_CAL_L470,
 	&app_read_REG_CAL_L560,
@@ -115,7 +115,7 @@ bool (*app_func_wr_pointer[])(void*) = {
 	&app_write_REG_SCREEN_ASSEMBLY_VERSION,
 	&app_write_REG_SCREEN_FW_VERSION_H,
 	&app_write_REG_SCREEN_FW_VERSION_L,
-	&app_write_REG_RESERVED1,
+	&app_write_REG_CAMERA_SN,
 	&app_write_REG_CAL_L410,
 	&app_write_REG_CAL_L470,
 	&app_write_REG_CAL_L560,
@@ -1117,12 +1117,12 @@ bool app_write_REG_SCREEN_FW_VERSION_L(void *a)
 /************************************************************************/
 /* REG_RESERVED1                                                    */
 /************************************************************************/
-void app_read_REG_RESERVED1(void) {}
-bool app_write_REG_RESERVED1(void *a)
+void app_read_REG_CAMERA_SN(void) {}
+bool app_write_REG_CAMERA_SN(void *a)
 {
-	uint16_t reg = *((uint16_t*)a);
+	uint64_t reg = *((uint64_t*)a);
 
-	app_regs.REG_RESERVED1 = reg;
+	app_regs.REG_CAMERA_SN = reg;
 	return true;
 }
 
